cdcb9b7a7c629829d628f3f6a815e332e87d9f5e
Roy Sarafov <sarafov.roy@gmail.com>
2026-01-18 19:24:36 +0200

    adding counter

--- counter/imem2.asm
+++ counter/imem2.asm
@@ -1,38 +1,47 @@
-# Counter Program - Core X
-# $r2 = Counter Address (0)
-# $r3 = Turn Address (1)
-# $r4 = Loop Counter (128)
-# $r5 = My ID (0, 1, 2, or 3)
-# $r6 = Next ID (1, 2, 3, or 0)
-# $r7 = Temporary for Data
-# $r8 = Temporary for Reading Turn
+# --- INITIALIZATION ---
+add $r2, $zero, $zero, 0        # R2 = 0
+add $r3, $zero, $imm, 1         # R3 = 1
+add $r4, $zero, $imm, 2         # R4 = 2 (My Expected Turn - UNIQUE TO CORE 2)
+add $r5, $zero, $imm, 4         # R5 = 4
+add $r6, $zero, $zero, 0        # R6 = 0
+add $r7, $zero, $imm, 128       # R7 = 128
+add $r10, $zero, $imm, 512      # R10 = 512
 
-# --- Initialization ---
-    add $r2, $zero, $imm, 0       # r2 = 0 (Counter Addr)
-    add $r3, $zero, $imm, 1       # r3 = 1 (Turn Addr)
-    add $r4, $zero, $imm, 128     # r4 = 128 (Loop Count)
+# --- MAIN LOOP ---
+Loop_Start:
+beq $imm, $r6, $r7, End_Seq
+add $zero, $zero, $zero, 0      # [DELAY SLOT]
 
-# !!! CHANGE THESE TWO LINES FOR EACH CORE !!!
-    add $r5, $zero, $imm, 2       # r5 = My ID (Set to 0, 1, 2, or 3)
-    add $r6, $zero, $imm, 3       # r6 = Next ID (Set to 1, 2, 3, or 0)
+# --- SPIN WAIT ---
+Spin_Wait:
+lw $r9, $r3, $imm, 0
+beq $imm, $r9, $r4, Critical
+add $zero, $zero, $zero, 0      # [DELAY SLOT]
+beq $imm, $zero, $zero, Spin_Wait
+add $zero, $zero, $zero, 0      # [DELAY SLOT]
 
-Loop:
-    beq $r4, $zero, $imm, Done    # If r4 == 0, finish
+# --- CRITICAL SECTION ---
+Critical:
+lw $r8, $r2, $imm, 0
+add $r8, $r8, $imm, 1
+sw $r8, $r2, $imm, 0
 
-WaitTurn:
-    lw $r8, $r3, $imm, 0          # Read Turn Variable (Addr 1)
-    bne $r8, $r5, $imm, WaitTurn  # If (Turn != MyID), keep waiting
+# --- PASS TOKEN ---
+lw $r9, $r3, $imm, 0
+add $r9, $r9, $imm, 1
+sw $r9, $r3, $imm, 0
 
-CriticalSection:
-    lw $r7, $r2, $imm, 0          # Read Counter
-    add $r7, $r7, $imm, 1         # Increment
-    sw $r7, $r2, $imm, 0          # Write Counter
+# --- UPDATE LOCAL STATE ---
+add $r4, $r4, $r5, 0
+add $r6, $r6, $imm, 1
+beq $imm, $zero, $zero, Loop_Start
+add $zero, $zero, $zero, 0      # [DELAY SLOT]
 
-PassTurn:
-    sw $r6, $r3, $imm, 0          # Write Next ID to Turn Variable
-
-    sub $r4, $r4, $imm, 1         # Decrement Loop Counter
-    beq $zero, $zero, $imm, Loop  # Jump back to Loop
-
-Done:
-    halt $zero, $zero, $zero, 0
+# --- FINALIZE ---
+End_Seq:
+lw $zero, $r10, $imm, 0
+halt $zero, $zero, $zero, 0     # Stop
+halt $zero, $zero, $zero, 0     # Stop
+halt $zero, $zero, $zero, 0     # Stop
+halt $zero, $zero, $zero, 0     # Stop
+halt $zero, $zero, $zero, 0     # Stop
